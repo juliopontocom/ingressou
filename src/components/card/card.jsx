280ba770bc48d5e867235cddce8f1b357a9b6a90
juliopontocom <146362643+juliopontocom@users.noreply.github.com>
2024-06-18 20:08:43 -0300

    dimensoes do card

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import styles from './card.module.css';
-import imagem1 from '../../assets/imagem1.png';
 
-const Card = () => {
+const Card = ({ backgroundImage }) => {
   return (
-    <div className={styles.card} style={{ backgroundImage: `url(${imagem1})` }}>
+    <div className={styles.card} style={{ backgroundImage: `url(${backgroundImage})` }}>
       <div className={styles.info}>
         <span className={styles.date}>15/06</span>
         <span className={styles.place}>Coolture</span>
--- src/components/scroll/scroll.jsx
+++ src/components/scroll/scroll.jsx
@@ -2,18 +2,20 @@ import React from 'react';
 import Carousel from '../carousel/carousel';
 import Card from '../card/card';
 import styles from './scroll.module.css';
+import imagem1 from '../../assets/imagem1.png';
+import imagem2 from '../../assets/imagem2.png';
+import imagem3 from '../../assets/imagem3.png';
+import imagem4 from '../../assets/imagem4.png';
 
 const Scroll = () => {
   return (
     <div className={styles.content}>
       <Carousel />
       <div className={styles.cardContainer}>
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card backgroundImage={imagem1} />
+        <Card backgroundImage={imagem2} />
+        <Card backgroundImage={imagem3} />
+        <Card backgroundImage={imagem4} />
       </div>
     </div>
   );
